79bf9ce1b23eef6d0aae068f52fdec3f088db8bf
Sergio Garcia <sgarher520@g.educaand.es>
2024-03-06 20:14:31 +0100

    validación de los formularios de registro y login implementados

--- frontend/src/app/login/login.page.ts
+++ frontend/src/app/login/login.page.ts
@@ -1,8 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { UsuarioService } from '../services/usuario/usuario.service';
 import { Router } from '@angular/router';
-import { RespuestaLogin } from '../models/RespuestaLogin';
-import { RespuestaUsuarioJwt } from '../models/RespuestaUsuarioJwt';
 
 @Component({
   selector: 'app-login',
@@ -12,26 +10,26 @@ import { RespuestaUsuarioJwt } from '../models/RespuestaUsuarioJwt';
 export class LoginPage {
   nombreUsuario: string = '';
   contrasena: string = '';
+  loginError: boolean = false; // Indica si hay un error de login
 
-  constructor(private usuarioService: UsuarioService, private router: Router ) { }
+  constructor(private usuarioService: UsuarioService, private router: Router) { }
 
   login() {
-    const loginData: RespuestaLogin = {
-      nombreUsuario: this.nombreUsuario,
-      contrasena: this.contrasena
-    };
-    this.usuarioService.login(this.nombreUsuario, this.contrasena).subscribe({
-      next: (res: RespuestaUsuarioJwt) => {
-        console.log('Login exitoso', res);
-        localStorage.setItem('token', res.token);
-        localStorage.setItem('userId', res.id); 
-        this.router.navigate(['/main/tareas']);
-      },
-      error: (e: any) => {
-        console.error('Error en el login', e);
-      }
-    });
-    
+    this.loginError = false;
+    if (this.nombreUsuario && this.contrasena) {
+      // Realiza la llamada al servicio de login
+      this.usuarioService.login(this.nombreUsuario, this.contrasena).subscribe({
+        next: (res) => {
+          console.log('Login exitoso', res);
+          localStorage.setItem('token', res.token);
+          // Redirige al usuario a la página principal o donde corresponda
+          this.router.navigate(['/main/tareas']);
+        },
+        error: (error) => {
+          console.error('Error en el login', error);
+          this.loginError = true; // Muestra mensaje de error en el template
+        }
+      });
+    }
   }
-
 }
